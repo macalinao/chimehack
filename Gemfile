2a4ff8f3bdde4120d21e496ed4b308422ed308b7
Ian Macalinao <me@ian.pw>
2015-07-12 11:09:30 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'dotenv'
+gem 'sinatra'
+gem 'google_directions'
+gem 'twilio-ruby'
+gem 'nokogiri'
+gem 'sanitize'
+gem 'googlestaticmap'
---
+++ app.rb
@@ -0,0 +1,62 @@
+require 'dotenv'
+Dotenv.load
+
+require 'sinatra'
+require 'google_directions'
+require 'twilio-ruby'
+require 'nokogiri'
+require 'sanitize'
+require 'googlestaticmap'
+
+post '/sms' do
+  body = params[:body]
+  to = params[:to]
+
+  sender = TwilioSender.new(body, to)
+  sender.make_sms
+  sender.make_map
+end
+
+class TwilioSender
+
+  attr_accessor :client, :places, :number
+  attr_reader :directions
+
+  def initialize(body, number)
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @places = body.split('to')
+    @number = number
+    init_directions
+  end
+
+  def init_directions
+    response = GoogleDirections.new(places.first, places.last)
+    doc = Nokogiri::XML(response.xml)
+    directions = ''
+    doc.xpath('/DirectionsResponse/route/leg/step/html_instructions').each do |step|
+      directions << Sanitize.fragment("#{step.content}\n")
+    end
+    @directions = directions
+  end
+
+  def make_sms
+    client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: number,
+      body: directions
+    )
+  end
+
+  def make_map
+    map = GoogleStaticMap.new
+    map.markers << MapMarker.new(:color => "blue", :location => MapLocation.new(:address => places.last))
+    image_url = map.url('http')
+    client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: number,
+      body: 'Here is a map.',
+      media_url: image_url
+    )
+  end
+
+end
